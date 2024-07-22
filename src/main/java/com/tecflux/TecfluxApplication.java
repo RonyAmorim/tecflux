dab790db1b4aefb4bc085fde2ee0746dd8bd75fc
Rony Amorim dos Reis <ronyamorimdosreis@gmail.com>
2024-07-22 19:51:39 -0300

    Initial commit

---
+++ src/main/java/com/tecflux/TecfluxApplication.java
@@ -0,0 +1,13 @@
+package com.tecflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecfluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecfluxApplication.class, args);
+	}
+
+}
