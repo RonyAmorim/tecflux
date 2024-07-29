04e4a98a52653941d1dbd4890c8bd4ae671a0fd0
Rony Amorim dos Reis <ronyamorimdosreis@gmail.com>
2024-07-29 16:01:19 -0300

    docs: Add API documentation with springdoc-openapi-ui
    - Added springdoc-openapi-ui dependency to pom.xml
    - Configured application to serve OpenAPI documentation
    - API documentation accessible at /swagger-ui/index.html
    
    This commit introduces API documentation using springdoc-openapi-ui, providing a user-friendly interface to explore and interact with the API endpoints.

---
+++ src/main/java/com/tecflux/config/OpenApiConfig.java
@@ -0,0 +1,26 @@
+package com.tecflux.config;
+
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Tecflux API")
+                        .description("API documentation for Tecflux")
+                        .version("v1.0")
+                        .contact(new Contact().name("Tecflux Team").email("contact@tecflux.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
+                .externalDocs(new ExternalDocumentation()
+                        .description("Tecflux Documentation")
+                        .url("https://tecflux.com/docs"));
+    }
+}
