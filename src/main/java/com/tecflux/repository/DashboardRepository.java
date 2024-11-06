36165ea5eb6d3256e6f95f877715d699d6eba68b
Nicole Ciccolani <109769301+JonathanCicero@users.noreply.github.com>
2024-11-06 16:10:01 -0300

    Update DashboardRepository.java

--- src/main/java/com/tecflux/repository/DashboardRepository.java
+++ src/main/java/com/tecflux/repository/DashboardRepository.java
@@ -5,22 +5,19 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import com.tecflux.repository.DashboardRepository;
 import com.tecflux.entity.Dashboard;
+
 import java.util.List;
 
 @Repository
 public interface DashboardRepository extends JpaRepository<Dashboard, Long> {
 
-    @Query("SELECT new com.tecflux.entity.Dashboard(" +
-       "SUM(CASE WHEN t.status = 1 THEN 1 ELSE 0 END), " +
-       "SUM(CASE WHEN t.status = 2 THEN 1 ELSE 0 END), " +
-       "SUM(CASE WHEN t.status = 3 THEN 1 ELSE 0 END)) " +
-       "FROM Ticket t " +
-       "WHERE t.empresaId = :empresaId")
-    Dashboard obterResumoChamadosPorEmpresa(Long empresaId);
+   @Query("SELECT new com.tecflux.entity.Dashboard(" +
+         "SUM(CASE WHEN t.status = 1 THEN 1 ELSE 0 END), " +
+         "SUM(CASE WHEN t.status = 2 THEN 1 ELSE 0 END), " +
+         "SUM(CASE WHEN t.status = 3 THEN 1 ELSE 0 END)) " +
+         "FROM Ticket t WHERE t.empresa.id = :empresaId")
+   Dashboard obterResumoChamadosPorEmpresa(Long empresaId);
 
-    @Query("SELECT u.id, COUNT(t) " +
-       "FROM Ticket t " +
-       "JOIN t.user u " +
-       "GROUP BY u.id")
-    List<Object[]> obterChamadosPorUsuario();
+   @Query("SELECT u.id, COUNT(t) FROM Ticket t JOIN t.user u GROUP BY u.id")
+   List<Object[]> obterChamadosPorUsuario();
 }
